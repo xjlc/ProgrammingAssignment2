040e7b6446475f25ecd55760685a34632b353d18
xjlc <swiftjlc@web.de>
2014-06-04 18:08:14 +0200

    First attempt at a solution
    works okay, but doesn’t yet provide error handling (checking for wrong
    argument types, whether matrix is invertible, etc.)

--- cachematrix.R
+++ cachematrix.R
@@ -5,7 +5,21 @@
 # makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 # First of a pair of functions that cache the inverse of a matrix. See also cacheSolve
 makeCacheMatrix <- function(x = matrix()) {
-
+    s <- NULL
+	## set is not strictly necessary here, as it is never used. Its function is to provide something similar to an OOP setter method
+    set <- function(y) {
+            x <<- y
+            s <<- NULL
+    }
+	## "getter", return value from closure (that entered the closure via the argument to the function makeCacheMatrix)
+	get <- function() x
+	## set the cached value for the inverse
+    setinverse <- function(inverse) s <<- inverse
+	## return the cached value for the inverse
+    getinverse <- function() s
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 # cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix. If the inverse has already been calculated (and the matrix has not changed), then cacheSolve retrieves the inverse from the cache.
@@ -13,5 +27,18 @@ makeCacheMatrix <- function(x = matrix()) {
 # Second of a pair of functions that cache the inverse of a matrix. See also makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	## has the mean inverse previously computed? If so, use the computed value
+    s <- x$getinverse()
+        if(!is.null(s)) {
+        message("getting cached data")
+        return(s)
+    }
+	## if not, get the data and compute the inverse
+    data <- x$get()
+    s <-  solve(data, ...)
+	
+	## cache the computed value
+    x$setinverse(s)
+    ## Return a matrix that is the inverse of 'x'
+    s
 }
